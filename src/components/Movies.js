849433a23bce11b491f380dab9cc62a3ea3e52b7
Gaurav Yadav <gk5480325@gmail.com>
2022-05-30 20:35:58 +0530

    A Disney_Clone_Website

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -11,13 +11,25 @@ function Movies() {
                               <img src="https://doms2cents.com/wp-content/uploads/2021/08/The-Simpsons-Season-33.jpg" />
                          </Wrap>
                          <Wrap>
-                              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5dtdtA7pyY-VLzj7GteZn_BH3vvR7hd_2uA&usqp=CAU" />
+                              <img src="https://doms2cents.com/wp-content/uploads/2021/08/The-Simpsons-Season-33.jpg" />
                          </Wrap>
                          <Wrap>
                               <img src="https://doms2cents.com/wp-content/uploads/2021/08/The-Simpsons-Season-33.jpg" />
                          </Wrap>
                          <Wrap>
-                              <img src="https://www2.pictures.zimbio.com/mp/WQWmxUKowttx.jpg" />
+                              <img src="https://doms2cents.com/wp-content/uploads/2021/08/The-Simpsons-Season-33.jpg" />
+                         </Wrap>
+                         <Wrap>
+                              <img src="https://doms2cents.com/wp-content/uploads/2021/08/The-Simpsons-Season-33.jpg" />
+                         </Wrap>
+                         <Wrap>
+                              <img src="https://doms2cents.com/wp-content/uploads/2021/08/The-Simpsons-Season-33.jpg" />
+                         </Wrap>
+                         <Wrap>
+                              <img src="https://doms2cents.com/wp-content/uploads/2021/08/The-Simpsons-Season-33.jpg" />
+                         </Wrap>
+                         <Wrap>
+                              <img src="https://doms2cents.com/wp-content/uploads/2021/08/The-Simpsons-Season-33.jpg" />
                          </Wrap>
                     </Content>
           </Container>
@@ -36,20 +48,27 @@ const Content = styled.div`
 
 const Wrap = styled.div`
      border-radius: 10px;
+     cursor: pointer;
      overflow: hidden;
      border: 3px solid rgba(249, 249, 249, 0.1);
      box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
-     rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+     rgb(0 0 0 / 73%) 0px 16px 10px -10px; 
+     transition : all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+     
 
      img{
           width: 100%;
           height: 100%;
           object-fit: cover;
+
      }
 
-     &:hover {
+     &:hover{
           transform: scale(1.05);
+          box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+          rgb(0 0 0 / 73%) 0px 16px 10px -10px;
           border-color: rgba(249, 249, 249, 0.8);
      }
+
 
 `
